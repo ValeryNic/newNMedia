504aa863a5fd54401416a4ccb62dc8021e90d46c
Valery <vn_ulanov@mail.ru>
2023-10-27 18:07:26 +0300

    Begin Adapter

---
+++ app/src/main/java/ru/netology/nmedia/presentation/activity/MainActivity.kt
@@ -0,0 +1,58 @@
+package ru.netology.nmedia.presentation.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import ru.netology.nmedia.adapter.PostAdapter
+import ru.netology.nmedia.R
+import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.ViewModel.PostViewModel
+
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewModel: PostViewModel by viewModels()
+        val adapter= PostAdapter{
+            viewModel.likeById(it.id)
+            viewModel.repostById(it.id)
+        }
+        binding.reciclerList.adapter=adapter
+        viewModel.data.observe(this){
+            posts ->
+            adapter.submitList(posts)
+        }
+
+    }
+
+
+
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+}
+
